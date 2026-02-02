8ac65d37d888c0353ca21e27555ec56d4b8984a6
dayeon <dayon0820@naver.com>
2026-02-02 18:10:43 +0900

    staff mobile screen

--- components/Staff/ProfileCard.jsx
+++ components/Staff/ProfileCard.jsx
@@ -3,10 +3,8 @@ import Image from "next/image"; // 1. next/image 임포트
 
 export default function ProfileCard({ member }) {
   return (
-    /* 1. aspect-[300/520] 제거: 내용에 맞게 짧아지도록 설정 */
-    <div className=" h-[430px] snap-start snap-always scroll-mt-[200px] bg-[var(--surface)] group flex flex-col  overflow-hidden transition-all hover:-translate-y-1 rounded-[var(--r-card)] border border-[var(--g6)]/10 shadow-sm">
-      {/* 2. 이미지: 정사각형을 유지하되 가로 폭이 좁아지면 세로도 같이 짧아짐 */}
-      <div className="relative aspect-square w-full overflow-hidden bg-[var(--g7)]">
+    <div className=" h-[317px] md:h-[430px] snap-start snap-always scroll-mt-[200px] bg-[var(--surface)] group flex flex-col  overflow-hidden transition-all hover:-translate-y-1 rounded-[var(--r-card)] border border-[var(--g6)]/10 shadow-sm">
+      <div className="relative w-full h-[157px] md:h-[230px] flex-shrink-0 overflow-hidden bg-[var(--g7)]">
         <Image
           src={`/STAFF/${member.name}.png`}
           alt={member.name}
@@ -16,24 +14,23 @@ export default function ProfileCard({ member }) {
         />
       </div>
 
-      {/* 3. 텍스트 영역: p-5에서 p-[16px]로 줄임 */}
-      <div className="flex flex-col py-[28px] px-[24px]">
-        <span className="text-[12px] font-bold text-[var(--primary)] mb-[8px]">
+      <div className="flex flex-col py-[18px] px-[16px] md:py-[24px] md:px-[20px]">
+        <span className="text-[10px] md:text-[12px] font-bold text-[var(--primary)] mb-[12px]">
           {member.role}
         </span>
 
-        {/* 4. 이름/학과 간격: mb-5에서 mb-[12px]로 축소 */}
-        <div className="flex flex-col gap-[8px] mb-[16px]">
-          <h3 className="text-[var(--text-strong)] text-[20px] font-bold leading-tight">
+        {/*  이름/학과 간격 축소 */}
+        <div className="flex flex-col gap-[6px] md:gap-[8px] mb-[12px] md:mb-[16px]">
+          <h3 className="text-[var(--text-strong)]text-[13px] md:text-[20px] font-bold leading-tight">
             {member.name}
           </h3>
-          <span className="text-[var(--text-muted)] text-[13px] font-medium">
+          <span className="text-[var(--text-muted)] text-[10px] md:text-[12px] font-medium">
             {member.dept}
           </span>
         </div>
 
-        <div className="rounded-[12px] rounded-tl-none bg-[var(--g7)] border border-[var(--g6)]/50 p-[10px]">
-          <p className="text-[var(--g3)] text-[13px] font-medium leading-[1.4] whitespace-pre-line line-clamp-2">
+        <div className="rounded-[6px] md:rounded-[8px] rounded-tl-none bg-[var(--g7)] border border-[var(--g6)]/50 p-[8px] md:p-[10px]">
+          <p className="text-[var(--g3)] text-[10px] md:text-[13px] font-medium leading-[1.4] whitespace-pre-line line-clamp-2">
             {member.desc}
           </p>
         </div>
