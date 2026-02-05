4e9686fdc4da885e918f09f8ded01749f456cb5e
meowtivator <02dosiu@gmail.com>
2026-02-06 00:50:52 +0900

    Feat: 제작한 컴포넌트 모집 안내 페이지에 반영

--- app/recruit/page.js
+++ app/recruit/page.js
@@ -1,3 +1,14 @@
-export default function Recruit() {
-  return <div className="pt-[88px]">모집안내</div>;
+import Curriculum from "@/components/Recruit/Curriculum";
+import Tracks from "../../components/Recruit/Tracks";
+import FAQ from "@/components/Recruit/FAQ";
+import { Notice } from "../../components/Recruit/Notice";
+export default function Page() {
+  return (
+    <main className="flex flex-col items-center justify-center gap-[100px]">
+      <Notice />
+      <Tracks />
+      <Curriculum />
+      <FAQ />
+    </main>
+  );
 }
