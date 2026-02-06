6462892c122a6d5d5a75dff9e8ff13b77fb0ea82
meowtivator <02dosiu@gmail.com>
2026-02-06 15:07:43 +0900

    Feat: 모집 일정 컴포넌트 제작 및 반영

--- app/recruit/page.js
+++ app/recruit/page.js
@@ -2,9 +2,11 @@ import Curriculum from "@/components/Recruit/Curriculum";
 import Tracks from "../../components/Recruit/Tracks";
 import FAQ from "@/components/Recruit/FAQ";
 import { Notice } from "../../components/Recruit/Notice";
+import Schedule from "@/components/Recruit/Schedule";
 export default function Page() {
   return (
     <main className="flex flex-col items-center justify-center gap-[100px]">
+      <Schedule />
       <Notice />
       <Tracks />
       <Curriculum />
